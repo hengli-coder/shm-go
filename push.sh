c8da928a9ffd9364a7edf9faa9d6f8421d498cc7
hengli <liheng__V@163.com>
2026-07-19 09:53:08 +0800

    chore: remove unused files (coverage, push.sh, run_all.bat, run_tests.bat)

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "d:/go/shm-go"
-git init
-git add -A
-git commit -m "Initial commit: shared memory cache service"
-git remote add origin https://github.com/hengli-coder/shm-go.git
-git branch -M main
-git push -u origin main
